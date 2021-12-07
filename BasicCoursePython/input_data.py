c08e7507f48e07c0498f0d441b1bcb7ce438ff1a
diego40g <diegopaznaula@hotmail.com>
2021-12-06 21:26:05 -0500

    input data, camel case of string to int, decimal to text

---
+++ BasicCoursePython/input_data.py
@@ -0,0 +1,15 @@
+#ingreso de datos
+numero1=input("Escribe un número: ")
+numero2=input("Escribe un número: ")
+print(numero1+numero2)
+#camel case
+numero1=int(numero1)
+numero2=int(numero2)
+print(numero1+numero2)
+
+print(int(4.5))
+numero_decimal=4.5
+print(numero_decimal)
+#convert decimal to string
+str(numero_decimal)
+print(numero_decimal)
