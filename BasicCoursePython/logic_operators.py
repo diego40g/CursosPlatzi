91986030257e459897d2b74612af57400b5f2d00
diego40g <diegopaznaula@hotmail.com>
2021-12-06 21:34:38 -0500

    logical operators with numbers and boolean

---
+++ BasicCoursePython/logic_operators.py
@@ -0,0 +1,20 @@
+es_estudiante=True
+trabaja=False
+
+print(es_estudiante and trabaja)
+print(es_estudiante or trabaja)
+print(not es_estudiante)
+
+numero1=5
+numero2=5
+print(numero1==numero2)
+numero3=7
+print(numero1==numero3)
+
+print(numero1!=numero3)
+
+print(numero1>numero3)
+print(numero1<numero3)
+
+print(numero1>=numero3)
+print(numero1<=numero2)
