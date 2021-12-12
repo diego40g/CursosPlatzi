6be8d35bacb7e30b28ee4088c9d17a52ca34ec8e
diego40g <diegopaznaula@hotmail.com>
2021-12-11 20:27:19 -0500

    string text and some funtions

---
+++ BasicCoursePython/string_operations.py
@@ -0,0 +1,15 @@
+nombre=input("¿Cuál es tu nombre?: ")
+##texto a mayusculas
+nombre.upper()
+##texto a minusculas
+nombre.lower()
+##primera letra mayuscula
+nombre.capitalize()
+##borra los espacios al final
+nombre.strip()
+##remplaza cual quier caracter por otro
+nombre.replace('a','A')
+##acceder a cada letra
+nombre[0]
+##tamanio de la cadena
+len(nombre)
