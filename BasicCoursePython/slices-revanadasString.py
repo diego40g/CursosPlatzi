a2ee5ca6ad604cd168c5e61d86f2f1f94f7b23e9
diego40g <diegopaznaula@hotmail.com>
2021-12-11 20:35:45 -0500

    slice to use string in array format

---
+++ BasicCoursePython/slices-revanadasString.py
@@ -0,0 +1,17 @@
+nombre='Ignacio'
+nombre[0]
+##arreglo con tres elementos IGN
+nombre[0:3]
+nombre[:3]
+##arreglo desde un indice hasta el final
+nombre[3:]
+##cantidad de pasos que voy saltando solo indices pares
+nombre[::2]
+##pasos de tres
+nombre[1:6:3]
+##imprimir todo
+nombre[::]
+##inicio y total de saltos que da
+nombre[1::2]
+##pasos negativos imprime el nombre de atras para adelante
+nombre[::-1]
