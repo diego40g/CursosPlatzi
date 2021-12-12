595cceba188fa8ba60c5092eaf1bd0696475a45b
diego40g <diegopaznaula@hotmail.com>
2021-12-11 19:31:45 -0500

    funtions to print, conditionals and return values

---
+++ BasicCoursePython/funtions.py
@@ -0,0 +1,31 @@
+"""def imprimir_mensaje():
+    print('Mensaje especial: ')
+    print('¡Estoy aprendiendo a usar funciones!')    
+
+imprimir_mensaje()
+imprimir_mensaje()
+imprimir_mensaje()"""
+
+"""def conversacion(mensaje):
+    print('Hola')
+    print('Cómo estas')
+    print(mensaje)
+    print('Adios')
+
+opcion=int(input('Elige una opción (1,2,3): '))
+if opcion==1:
+    conversacion('Elegiste la opción 1')
+elif opcion==2:
+    conversacion('Elegiste la opción 2')
+elif opcion==3:
+    conversacion('Elegiste la opción 3')
+else:
+    print('Solo opciones del 1 al 3')"""
+
+def suma (a,b):
+    print('Se suman dos números')
+    resultado = a+b
+    return resultado
+
+sumatoria=suma(1,4)
+print(sumatoria)
