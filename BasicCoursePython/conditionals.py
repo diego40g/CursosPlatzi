835af2247937f4a3e6d791e0785043d49108d341
diego40g <diegopaznaula@hotmail.com>
2021-12-11 19:06:18 -0500

    how to use conditionals if-elif-else

---
+++ BasicCoursePython/conditionals.py
@@ -0,0 +1,14 @@
+"""edad=int(input("Escribe tu edad: "))
+if edad<18:
+    print('Eres menor de edad')
+else:
+    print('Eres mayor de edad')"""
+    #pass#palabra clavle para rellenar codigo despues
+
+numero=int(input("Escribe un número: "))
+if numero>5:
+    print('Es mayor a 5')
+elif numero==5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
