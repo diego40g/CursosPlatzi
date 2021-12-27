2ec77e72fe661a1701fa7da6cc9e8f58409051ec
diego40g <diegopaznaula@hotmail.com>
2021-12-27 02:05:15 +0000

    search two large number into the array python

---
+++ BasicCourseAlgorithmics/secondNumber.py
@@ -0,0 +1,22 @@
+print("""El programa busca el primer y segundo numero mayor dentro de un arreglo""")
+arreglo=[969,666,1,8,10,-9,-500,0,500]
+
+numMayor1=arreglo[0]
+numMayor2=arreglo[1]
+
+if numMayor1 < numMayor2:
+  auxiliar=numMayor1
+  numMayor1=numMayor2
+  numMayor2=auxiliar
+
+i=2
+while i<len(arreglo):
+  if arreglo[i] > numMayor2:
+    if numMayor2 > numMayor1: 
+      numMayor1=arreglo[i]
+    else:
+      numMayor2=arreglo[i]  
+  i+=1
+
+print(numMayor1)
+print(numMayor2)
