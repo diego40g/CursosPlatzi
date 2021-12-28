2d7cbb467c8ede99e83e0698b02927b1a65095ee
diego40g <diegopaznaula@hotmail.com>
2021-12-27 20:27:44 -0500

    first line code of javascript in browser

---
+++ BasicCoursePrograming/1. primeraLineaCodigo.js
@@ -0,0 +1,9 @@
+alert("Mi nombre es Diego");
+var nombre="Diego";
+alert("Mi nombre es "+nombre);
+
+nombre="Juana la Alpaca";
+alert("Mi nombre es "+nombre);
+
+nombre=prompt("Cual es tu nombre?");
+alert("Mi nombre es "+nombre);
