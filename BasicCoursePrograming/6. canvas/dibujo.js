e2ee805264831256e02ecb90068117779d713f39
diego40g <diegopaznaula@hotmail.com>
2022-01-01 18:56:27 -0500

    do function draw in dibujo.js

--- BasicCoursePrograming/6. canvas/dibujo.js
+++ BasicCoursePrograming/6. canvas/dibujo.js
@@ -1,9 +1,14 @@
 var d=document.getElementById("dibujito");
 var lienzo=d.getContext("2d");
 
-lienzo.beginPath();
-lienzo.strokeStyle="red";//atributo color del canvas 
-lienzo.moveTo(50,100);//mover el lapiz a donde va a comer la cajada del lienzo
-lienzo.lineTo(250,2);//trazar la linea
-lienzo.stroke();//dibujar el trazo 
-lienzo.closePath();//cerramos el trazo
+dibujarLinea("pink",10,300,220,10);
+dibujarLinea("yellow",300,10,10,220);
+
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final){
+    lienzo.beginPath();
+    lienzo.strokeStyle=color;//atributo color del canvas 
+    lienzo.moveTo(x_inicial,y_inicial);//mover el lapiz a donde va a comer la cajada del lienzo
+    lienzo.lineTo(x_final,y_final);//trazar la linea
+    lienzo.stroke();//dibujar el trazo 
+    lienzo.closePath();//cerramos el trazo
+}
