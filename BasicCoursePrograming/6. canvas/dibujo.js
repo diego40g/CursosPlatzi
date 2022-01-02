b61567127b21e57ac84e43d2798c79e0a2bc005f
diego40g <diegopaznaula@hotmail.com>
2022-01-01 20:18:17 -0500

    Rose with functions

--- BasicCoursePrograming/6. canvas/dibujo.js
+++ BasicCoursePrograming/6. canvas/dibujo.js
@@ -1,8 +1,28 @@
 var d=document.getElementById("dibujito");
 var lienzo=d.getContext("2d");
 
-dibujarLinea("pink",10,300,220,10);
-dibujarLinea("yellow",300,10,10,220);
+var lineas=30;
+var l=0;
+var yi, xf;
+var colorcito="#FAA"
+
+/*while(l<lineas){
+    l++;*/
+for(l=0;l<lineas;l++){
+    xi=l*10;
+    yi=l*10;
+    xf=10*(l+1);
+    yf=10*(l+1);
+    dibujarLinea(colorcito,0,yi,xf,300);
+    dibujarLinea(colorcito,xi,300,300,300-yf);
+    dibujarLinea(colorcito,0,yi,300-xf,0);
+    dibujarLinea(colorcito,300,yi,xf,0);
+    console.log("Linea "+l)
+}
+
+dibujarLinea(colorcito,1,1,1,299);
+dibujarLinea(colorcito,1,299,299,299);
+
 
 function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final){
     lienzo.beginPath();
