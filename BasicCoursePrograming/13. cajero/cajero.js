4d28fe3d39967d33b7d97118d52194010a94151f
diego40g <diegopaznaula@hotmail.com>
2022-01-03 17:10:21 -0500

    add image, rest money in ATM and show total of atm

--- BasicCoursePrograming/13. cajero/cajero.js
+++ BasicCoursePrograming/13. cajero/cajero.js
@@ -1,22 +1,29 @@
 class Billete{
-    constructor(valor,cantidad){
+    constructor(valor,cantidad,imagen){
         this.valor=valor;
         this.cantidad=cantidad;
+        this.imagen=imagen;
     }
 }
 
 var entregado=[];
 var caja=[];
-caja.push(new Billete(125,10));
-caja.push(new Billete(50,10));
-caja.push(new Billete(20,30));
-caja.push(new Billete(10,10));
-caja.push(new Billete(5,10));
+caja.push(new Billete(100,Math.floor(Math.random()*10),"/assets/100.png"));
+caja.push(new Billete(50,Math.floor(Math.random()*10),"/assets/50.png"));
+caja.push(new Billete(20,Math.floor(Math.random()*10),"/assets/20.png"));
+caja.push(new Billete(10,Math.floor(Math.random()*10),"/assets/10.png"));
+caja.push(new Billete(5,Math.floor(Math.random()*10),"/assets/5.png"));
+caja.push(new Billete(1,Math.floor(Math.random()*10),"/assets/1.png"));
 
 var dinero;
 var div;
 var papeles;
+var total=0;
+for (bi of caja){
+    total+=bi.cantidad*bi.valor;
+}
 
+var totalCaja=document.getElementById("totalCaja");
 var boton=document.getElementById("retirar");
 var resultado=document.getElementById("resultado")
 boton.addEventListener("click", entregarDinero);
@@ -33,7 +40,7 @@ function entregarDinero(){
             }else{
                 papeles=div;
             }
-            entregado.push(new Billete(bi.valor,papeles));
+            entregado.push(new Billete(bi.valor,papeles,bi.imagen));
             dinero=dinero-(bi.valor*papeles);
         }
     }
@@ -41,10 +48,19 @@ function entregarDinero(){
     {
         for(var e of entregado){
             if(e.cantidad!=0){
-                resultado.innerHTML+=e.cantidad+" billetes de $"+e.valor+"<br/>";
+                // resultado.innerHTML+=e.cantidad+" billetes de $"+e.valor+"<br/>";
+                for(var i=0;i<e.cantidad;i++){
+                    resultado.innerHTML+="<img src='"+e.imagen+"' style='width: 150px;'/>";
+                }
+                total-=e.cantidad*e.valor;
             }
         }
     }else{
         resultado.innerHTML="Soy un cajero pobre y no tengo suficiente dinero :'(";
     }
+    mostrarTotalCaja(total);
+}
+
+function mostrarTotalCaja(total){
+    totalCaja.innerHTML="El total de la caja es $"+total+"<br/>";
 }
