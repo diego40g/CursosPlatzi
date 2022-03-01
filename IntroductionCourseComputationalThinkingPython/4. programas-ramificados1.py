8a245e81f1dd1ff0577b9c76e824212744decda3
diego40g <diegopaznaula@hotmail.com>
2022-03-01 00:15:46 -0500

    programas ramificados

---
+++ IntroductionCourseComputationalThinkingPython/4. programas-ramificados1.py
@@ -0,0 +1,30 @@
+#los ... significa que se esta por agregar codigo
+
+#if <condicion>:
+#    <expresion>
+
+if 3>2:
+    print('3 es mayor que 2')
+
+#if <condicion>:
+#    <expresion>
+#else:
+#    <expresion>
+if 5<=10:
+    ...
+else:
+    print('5 no es mayor o igual que 10')
+
+#if <condicion>:
+#    <expresion>
+#elif <condicion>:
+#    <expresion>
+#else:
+#    <expresion>
+
+if 4>5:
+    ...
+elif 4<5:
+    print('4 es menor que 5')
+else:
+    ...
---
+++ IntroductionCourseComputationalThinkingPython/5. programas-ramificados2.py
@@ -0,0 +1,9 @@
+num_1=int(input('Escoge un entero:'))
+num_2=int(input('Escoge un entero:'))
+
+if num_1>num_2:
+    print('El primer numero es mayor que el segundo')
+elif num_1<num_2:
+    print('El segundo numero es mayor que el primero')
+else:
+    print('Los dos numero son iguales')
