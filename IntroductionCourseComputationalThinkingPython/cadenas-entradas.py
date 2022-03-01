cf7a4d3ba193aad901db4a167484b5da018d96c8
diego40g <diegopaznaula@hotmail.com>
2022-02-28 23:46:25 -0500

    como funcionan las cadenas

---
+++ IntroductionCourseComputationalThinkingPython/cadenas-entradas.py
@@ -0,0 +1,18 @@
+'123'
+'123' * 3
+'123' + '456'
+('Hip'*3)+' '+'Hurra'
+f'{"Hip " * 3} Hurra'
+
+my_str='Platzi'
+len(my_str)
+my_str[0]  #notación indices 
+my_str[0:2:1]      #notación rebanadas SLICING [inicio:fin:pasos]
+my_str[2:]
+my_str[:3]
+my_str[:-2]
+my_str[::2]
+
+'Yo amo a '+my_str          #imprimir cadena
+f'Yo amo a {my_str}'        #imprimir cadena
+f'Yo amo a {my_str}, ' * 100  #imprimir 100 veces la cadena
