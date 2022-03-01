4fbb0e5b25244a137ec01ab08f6c01cc0a01ce7b
diego40g <diegopaznaula@hotmail.com>
2022-02-28 23:27:13 -0500

    objetos, expresiones y tipos de numeros

---
+++ IntroductionCourseComputationalThinkingPython/literal-operador-literal.py
@@ -0,0 +1,48 @@
+1+2
+
+# 1 3.0 mala sintaxis
+# 5 / 'Platzi' no se pude dividir para una cadena
+5 * 'Platzi'
+
+#imprimir informacion en consola
+print('Hola, mundo!!!')
+
+#<objeto> <operador> <objeto> es una expresión que devuelve un
+#valor
+
+'Platzi'+'Rocks!'
+#PlatzoRocks
+
+2+2
+#4
+
+#persona+persona
+#pareja                  algo ideal que tocara aprender
+
+my_int=1
+my_float=1.0
+my_bool=True
+my_none=None
+
+#recresa el tipo de objeto
+type(my_int)
+type(my_float)
+type(my_bool)
+type(my_none)
+
+#operadores
+1+2
+2-5
+2.0*3
+6//2
+6//4 #residuo
+6/4
+7%2 #modulo
+2**3
+
+#area de un triangulo
+base=2
+altura=4
+area=(base*altura)/2
+
+
