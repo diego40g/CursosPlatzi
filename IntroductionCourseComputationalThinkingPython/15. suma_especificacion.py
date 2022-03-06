4913e8542df6ba9c962da58dc63866bda70254e2
diego40g <diegopaznaula@hotmail.com>
2022-03-06 16:10:57 -0500

    docs of functions in python and how to read the documentaion on functions

---
+++ IntroductionCourseComputationalThinkingPython/15. suma_especificacion.py
@@ -0,0 +1,19 @@
+def suma(a,b):
+    """Suma dos valores a y b
+    param int a cualquier entero
+    param int b cualquier entero
+    returns la sumatoria de a y b
+    """
+    total=a+b
+    return total
+
+help(suma)
+
+def a(cualquier_parametro):
+    """Descripcion de lo que hace nuestra funcion
+        cuaquier_parametro int cualquier entero
+        returns cualquier_parametro + 5
+    """
+    return cualquier_parametro+5
+
+help(a)
