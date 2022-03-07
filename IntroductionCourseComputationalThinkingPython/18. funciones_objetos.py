1ad55c17c4749345f188c6c2b9adb06f67de5bca
diego40g <diegopaznaula@hotmail.com>
2022-03-06 20:25:53 -0500

    data struct with functions

--- IntroductionCourseComputationalThinkingPython/18. funciones_objetos.py
+++ IntroductionCourseComputationalThinkingPython/18. funciones_objetos.py
@@ -1,3 +1,4 @@
+#funciones como objetos
 def multiplicar_por_dos(n):
     return n*2
 
@@ -11,9 +12,26 @@ def aplicar_operacion(funcion,numeros):
         resultados.append(resultado)
     return resultados
 
+#funciones como estructura de datos
+def aplicar_operaciones(num):
+    operaciones=[abs,float]
+    print(operaciones)
+    resultado=[]
+    for operacion in operaciones:
+        resultado.append(operacion(num))
+    return resultado
+
 if __name__=="__main__":
+    #funciones como objetos
     nums=[1,2,3]
     print(f"Arreglo original {nums}")
     print(f"Arreglo funcion por dos {aplicar_operacion(multiplicar_por_dos, nums)}")
     print(f"Arreglo funcion mas dos {aplicar_operacion(sumar_dos, nums)}")
     
+    #funciones como expresiones
+    print("Funciones en expresiones: ")
+    sumar=lambda x,y:x+y
+    print(f"Variable suma {sumar(2,3)}")
+
+    #funciones como estrucutura de datos
+    print(aplicar_operaciones(-3))
