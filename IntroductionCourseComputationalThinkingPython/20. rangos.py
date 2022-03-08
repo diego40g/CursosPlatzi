a8ac507cfa755459540d2440b44a83e3d3a9c5e7
diego40g <diegopaznaula@hotmail.com>
2022-03-08 00:06:21 -0500

    ranges and number evens and odds

--- IntroductionCourseComputationalThinkingPython/20. rangos.py
+++ IntroductionCourseComputationalThinkingPython/20. rangos.py
@@ -6,5 +6,24 @@ print(type(my_range))
 for i in my_range:
     print(i)
 
+#GEneran la misma secuencia de valores
 my_range=range(0,7,2)
 my_other_range=range(0,8,2)
+#comparación de igualdad valores
+print(my_range==my_other_range)
+for i in my_range:
+    print(i)
+
+for i in my_other_range:
+    print(i)
+
+#identificador para variables
+print(id(my_range))
+print(id(my_other_range))
+
+#comapracion de igualdad dentro de objetos
+print(my_range is my_other_range)
+
+#generar numeros pares
+for i in range(0,101,2):
+    print(i)
---
+++ IntroductionCourseComputationalThinkingPython/21. cuarto_desafio.py
@@ -0,0 +1,3 @@
+#generar un rango con todos los numeros nones(impares) del 1 al 99
+for i in range(1,101,2):
+    print(i)
