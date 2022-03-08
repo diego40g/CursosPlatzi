80bcbfb9a6d197917d9ae8d34d577ed7a32fdf92
diego40g <diegopaznaula@hotmail.com>
2022-03-07 23:59:37 -0500

    examples ranges

---
+++ IntroductionCourseComputationalThinkingPython/20. rangos.py
@@ -0,0 +1,10 @@
+#range(comienzo,fin,pasos)
+my_range=range(1,5)
+print(type(my_range))
+
+#es no inclusivo no tenemos el numero final es decir el cinco
+for i in my_range:
+    print(i)
+
+my_range=range(0,7,2)
+my_other_range=range(0,8,2)
