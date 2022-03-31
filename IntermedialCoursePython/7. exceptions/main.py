25b31d08f5b8ff69439f91f267538320a8d5c1f5
diego40g <diegopaznaula@hotmail.com>
2022-03-30 23:36:23 -0500

    implements raise

--- IntermedialCoursePython/7. exceptions/main.py
+++ IntermedialCoursePython/7. exceptions/main.py
@@ -1,8 +1,14 @@
 def palindrome(string):
-    return string==string[::-1]
+    try:
+        if len(string)==0:
+            raise ValueError("No se puede ingresar una cadena vacía")
+        return string==string[::-1]
+    except ValueError as ve:
+        print(ve)
+        return False
 
 if __name__=="__main__":
     try:
-        print(palindrome(1))
+        print(palindrome(""))
     except TypeError:
         print("Solo se puede ingresar strings")
