8d433ad8b34e5fbdf627f0059b12fdf587ea8042
diego40g <diegopaznaula@hotmail.com>
2022-04-10 17:52:47 -0500

    Add inheritance of car in new class of types uber

---
+++ BasicCourseOOP/1. CursoPOOUber/Java/UberPool.java
@@ -0,0 +1,10 @@
+public class UberPool extends Car{
+    String brand;
+    String model;
+
+    public UberPool(String license, Account driver, String brand, String model){
+        super(license, driver);
+        this.brand=brand;
+        this.model=model;
+    }
+}
---
+++ BasicCourseOOP/1. CursoPOOUber/Java/UberVan.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+public class UberVan extends Car{
+    Map<String, Map<String,Integer>> typeCarAccepted;
+    ArrayList<String> seatsMaterial;
+    
+    public UberVan(String license, Account driver, 
+    Map<String, Map<String,Integer>> typeCarAccepted,
+    ArrayList<String> seatsMaterial){
+        super(license, driver);
+        this.typeCarAccepted=typeCarAccepted;
+        this.seatsMaterial=seatsMaterial;
+    }
+}
