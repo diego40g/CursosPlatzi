9a55f6281c22a519642dc6126e8a7d2ab4d2d07d
diego40g <diegopaznaula@hotmail.com>
2022-04-10 18:18:09 -0500

    create ubers

--- BasicCourseOOP/1. CursoPOOUber/PHP/index.php
+++ BasicCourseOOP/1. CursoPOOUber/PHP/index.php
@@ -1,7 +1,15 @@
 <?php
-    require_once('Car.php');
-    require_once('Account.php');
+    require_once('car.php');
+    require_once('account.php');
+    require_once('uberX.php');
+    require_once('uberPool.php');
 
-    $car=new Car ("PAQ6521",new Account("Juan Perez","1717549845"));
-    $car->PrintDataCar();
+    // $car=new Car ("PAQ6521",new Account("Juan Perez","1717549845"));
+    // $car->PrintDataCar();
+
+    $uberX=new UberX("PFS4563", new Account("Juan Perez","1723458932"), "Ford", "Mustan");
+    $uberX->PrintDataCar();
+
+    $uberPool=new UberPool("PTY9887", new Account("Pedro Rosero","1798654521"),"Toyota","Corona");
+    $uberPool->PrintDataCar();
 ?>
