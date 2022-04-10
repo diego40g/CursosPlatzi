eed849006c76d0d2a8c1daa91f7844bd87fe0355
diego40g <diegopaznaula@hotmail.com>
2022-04-10 18:48:43 -0500

    create  uberX example

--- BasicCourseOOP/1. CursoPOOUber/JS/index.js
+++ BasicCourseOOP/1. CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
 var car=new Car("PFG6587",new Account("Juan Perez","1728395465"))
 car.passenger=4;
 car.printDataCar();
+
+var uberX=new UberX("PRT5454",new Account("Pedro Rosero","1787452198"),"Ford","Mustan")
+uberX.passenger=4;
+uberX.printDataCar();
