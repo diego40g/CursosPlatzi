dbf499a4376574a6f8d157fe22e8c486f79e2f77
diego40g <diegopaznaula@hotmail.com>
2022-04-10 15:20:07 -0500

    construct of car and solve error in route of arraylist

--- BasicCourseOOP/1. CursoPOOUber/Java/Route.java
+++ BasicCourseOOP/1. CursoPOOUber/Java/Route.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Route{
     Integer id;
     ArrayList<Double> start;
--- BasicCourseOOP/1. CursoPOOUber/Java/main.java
+++ BasicCourseOOP/1. CursoPOOUber/Java/main.java
@@ -1,5 +1,15 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Car car = new Car();
+        car.license="PQE4651";
+        car.driver="Juan Perez";
+        car.passengers=4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license="PMA3644";
+        car2.driver="Pedro Rosero";
+        car2.passengers=3;
+        car2.printDataCar();
     }
 }
