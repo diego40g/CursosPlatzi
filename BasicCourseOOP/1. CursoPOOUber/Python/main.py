e7c2225a6dbe8d9ef3a6468701feecb14f652855
diego40g <diegopaznaula@hotmail.com>
2022-04-10 18:37:06 -0500

    create example of uberX and uberPool

--- BasicCourseOOP/1. CursoPOOUber/Python/main.py
+++ BasicCourseOOP/1. CursoPOOUber/Python/main.py
@@ -1,7 +1,13 @@
 from car import Car
 from account import Account
+from uberX import UberX
+from uberPool import UberPool
 
 if __name__ == "__main__":
-    car=Car("PTD9631", Account("Pedro Rosero","1745876523"))
-    print(vars(car))
-    print(vars(car.driver))
+    # car=Car("PTD9631", Account("Pedro Rosero","1745876523"))
+    # print(vars(car))
+    # print(vars(car.driver))
+    uberX=UberX("PLL","Pedro Rosero","Toyota","Corona")
+    print(vars(uberX))
+    uberPool=UberPool("PHJ2332","Juan Perez","Ford","Mustan")
+    print(vars(uberPool))
