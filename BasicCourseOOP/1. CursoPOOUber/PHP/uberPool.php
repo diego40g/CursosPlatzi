9ffc06bca349dfa2e2f90ee98b32feae80d1c60b
diego40g <diegopaznaula@hotmail.com>
2022-04-10 18:10:23 -0500

    solve error of ; and add a parameters in contruct

--- BasicCourseOOP/1. CursoPOOUber/PHP/uberPool.php
+++ BasicCourseOOP/1. CursoPOOUber/PHP/uberPool.php
@@ -1,11 +1,11 @@
 <?php
-    require_once('car.php')
+    require_once('car.php');
     class UberPool extends Car {
         public $brand;
         public $model;
 
         public function __construct($license,$driver,$brand,$model){
-            parent::__construct
+            parent::__construct($license,$driver);
             $this->brand=$brand;
             $this->model=$model;
         }
--- BasicCourseOOP/1. CursoPOOUber/PHP/uberVan.php
+++ BasicCourseOOP/1. CursoPOOUber/PHP/uberVan.php
@@ -1,11 +1,11 @@
 <?php
-    require_once('car.php')
+    require_once('car.php');
     class UberVan extends Car {
         public $typeCarAccepted;
         public $seatsMaterial;
 
         public function __construct($license,$driver,$brand,$model){
-            parent::__construct
+            parent::__construct($license,$driver);
             $this->typeCarAccepted=$typeCarAccepted;
             $this->seatsMaterial=$seatsMaterial;
         }
