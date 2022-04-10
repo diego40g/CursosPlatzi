8f6f864422164ac44f67a3f5bd76cc108a8e5993
diego40g <diegopaznaula@hotmail.com>
2022-04-10 16:01:05 -0500

    new metho for class

--- BasicCourseOOP/1. CursoPOOUber/JS/Car.js
+++ BasicCourseOOP/1. CursoPOOUber/JS/Car.js
@@ -1,12 +1,14 @@
-function Car(license, driver){
-    this.id;
-    this.license=license;
-    this.driver=driver;
-    this.passengers;
-}
+class Car {
+    constructor(license,driver){
+        this.id;
+        this.license=license;
+        this.driver=driver;
+        this.passenger;
+    }
 
-Car.prototype.printDataCar=function(){
-    console.log(this.driver)
-    console.log(this.driver.name)
-    console.log(this.driver.document);
+    printDataCar(){
+        console.log(this.driver);
+        console.log(this.driver.name);
+        console.log(this.driver.document);
+    }
 }
