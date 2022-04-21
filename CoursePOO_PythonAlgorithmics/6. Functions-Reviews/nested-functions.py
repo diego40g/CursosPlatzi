f2d1dfd3d9be7ef690ebf624d1d4838c150cbd69
diego40g <diegopaznaula@hotmail.com>
2022-04-20 22:52:40 -0500

    crea functiosn into the other functions and call to see the order of execute

---
+++ CoursePOO_PythonAlgorithmics/6. Functions-Reviews/nested-functions.py
@@ -0,0 +1,16 @@
+def funcion_mayor():
+    print("Esta es una fucnion mayor y su mensaje de salida.")
+
+    def librerias():
+        print("Algunas librerías de Python son: Scikit-learn, Numpy y Tensorflow.")
+    def frameworks():
+        print("Algunos frameworks de Python son: Django, Dash y Flask.")
+
+    frameworks()
+    librerias()
+
+def run():
+    funcion_mayor()
+
+if __name__=="__main__":
+    run()
