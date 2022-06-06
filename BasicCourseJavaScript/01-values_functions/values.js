0ac70f97748bc2379dd867926173b8943b2f298d
diego40g <diegopaznaula@hotmail.com>
2022-06-05 21:55:18 -0500

    values and the type of this values

---
+++ BasicCourseJavaScript/01-values_functions/values.js
@@ -0,0 +1,10 @@
+//primitivos
+40 //valor tipo numero
+"Diego Paz" //valor tipo string
+true-false //valor tipo boolean
+null//valor empty o vacio
+undefined
+
+//objetos
+[1,2,3]//arreglos arrays
+{ nombre: "Diego" }
