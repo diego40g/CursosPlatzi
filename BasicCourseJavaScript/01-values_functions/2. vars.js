aa717a90cbf3afd1e023273dcf30bd73ee569ca3
diego40g <diegopaznaula@hotmail.com>
2022-06-05 22:03:34 -0500

    vars primitive and objects

---
+++ BasicCourseJavaScript/01-values_functions/2. vars.js
@@ -0,0 +1,16 @@
+var nombre="Diego";
+console.log(nombre);
+//estado de declarar una varaible
+var edad;
+console.log(edad);
+//estado de inicializacion la variable
+edad=24;
+console.log(edad)
+
+var elementos=["Computadora","Celular"];
+
+var persona={
+    nombre: "Diego",
+    edad: 24
+}
+console.log(persona);
