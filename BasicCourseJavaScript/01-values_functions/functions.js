2ed683b0d8186eb61d3eebac08850c4792b75705
diego40g <diegopaznaula@hotmail.com>
2022-06-07 23:10:26 -0500

    functions type, the declaration and call

---
+++ BasicCourseJavaScript/01-values_functions/functions.js
@@ -0,0 +1,13 @@
+//Declarativas (inicaliza un valor y lo guarda en memoria)
+
+function miFuncionDeclarativa(){
+    return 3;
+}
+miFuncionDeclarativa();
+
+//Expresión (tambien conocidas como anonimas)
+
+var miFuncionExpresion = function(a,b){
+    return a+b;
+}
+miFuncionExpresion();
