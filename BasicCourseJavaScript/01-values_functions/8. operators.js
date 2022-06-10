9a45e5dc7fc525017334d77ae7ae92005ddc8cd7
diego40g <diegopaznaula@hotmail.com>
2022-06-10 00:00:04 -0500

    operators

---
+++ BasicCourseJavaScript/01-values_functions/8. operators.js
@@ -0,0 +1,22 @@
+/*operadores artiméticos, matematicos, binario porque es entre dos varaibles*/
+3+2;//suma
+50-10;//resta
+10*20;//multi
+20/2;//divi
+"Diego"+"Paz";//concatena
+/*operadores unitarios porque trabajan con un solo valor*/
+var negacion=!false
+/*operadores de asignacion*/
+var a=1;//asignado el valor a la inicializacion 
+/*operadores de comparación*/
+3=="3";//compara los valores (el contenido) devuelve TRUE
+3==="3";//valida que los dos valores sean completamente iguales en tipo y contenido del valor FALSE
+
+5<3;
+5>3;
+5<=6;
+5>=6;
+/*OPERADORES logicos*/
+a && b;//Y AND && debe cumplir las dos
+a || b;//O OR || si alguno de estas es verdad o las dos
+/*
