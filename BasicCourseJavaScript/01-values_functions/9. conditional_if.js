83a7e45ed55616ebd0cfa2f57c1783f2a26132b3
diego40g <diegopaznaula@hotmail.com>
2022-06-10 23:55:02 -0500

    conditional if, else and else if

---
+++ BasicCourseJavaScript/01-values_functions/9. conditional_if.js
@@ -0,0 +1,15 @@
+if (true) {
+    console.log("Hola dentro if");
+}else{
+    console.log("Hola dentro else");
+}
+
+
+var edad=18;
+if(edad===18){
+    console.log("Puedes votar, es tu primera votación.");
+}else if(edad>18){
+    console.log("Puedes votar de nuevo");
+}else{
+    console.log("No puedes votar eres muy joven aun.");
+}
