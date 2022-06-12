0fb929c93c966cd1711a4488864b760b9ba34238
diego40g <diegopaznaula@hotmail.com>
2022-06-11 20:28:26 -0500

    example to use switch with 3 cases

---
+++ BasicCourseJavaScript/01-values_functions/12. switch.js
@@ -0,0 +1,18 @@
+const numero=1;
+switch (numero) {
+    case 1:
+        console.log("caso 1");
+        console.log("Soy uno!!!");
+        break;
+    case 10:
+        console.log("caso 10");
+        console.log("Soy un diez!!!");
+        break;
+    case 100:
+        console.log("caso 100");
+        console.log("Soy un cien!!!");
+        break;
+    default:
+        console.log("Default");
+        console.log("No soy nada de 1, 10 o 100");
+}
