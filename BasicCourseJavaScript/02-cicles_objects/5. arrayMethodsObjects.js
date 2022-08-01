2e426cc6c1d488960421515dba82b2376af978e1
diego40g <diegopaznaula@hotmail.com>
2022-07-31 19:07:45 -0500

    method find, some and foreach into objects

--- BasicCourseJavaScript/02-cicles_objects/5. arrayMethodsObjects.js
+++ BasicCourseJavaScript/02-cicles_objects/5. arrayMethodsObjects.js
@@ -4,6 +4,7 @@ const articulos = [
     {nombre:"Libro",costo:320},
     {nombre:"Celular",costo:10000},
     {nombre:"Laptop",costo:20000},
+    {nombre:"Laptop",costo:15000},
     {nombre:"Teclado",costo:500},
     {nombre:"Audifonos",costo:1700}
 ]
@@ -18,4 +19,25 @@ const articulosFiltrados=articulos.filter((articulos)=>{
 //devuelve cierto atributo de todos los atributos
 const nombreArticulo=articulos.map((articulos)=>{
     return articulos.nombre
+})
+
+//find
+//devuelve la primera coincidencia 
+const encuentraArticulo=articulos.find((articulo)=>{
+    return articulo.nombre==="Laptop"
+})
+
+//forEach
+//bucle dentro de todo los articulos, usado para filtrar propiedades o valores
+articulos.forEach((articulo)=>{
+    console.log(articulo.nombre)
+    if(articulo.nombre==="Laptop"){
+        console.log("laptop")
+    }
+})
+
+//some
+//Devuelve true o false que devuelve si existe o cumple la condición
+const articulosBaratos=articulos.some((articulo)=>{
+    return articulo.costo<=3000
 })
