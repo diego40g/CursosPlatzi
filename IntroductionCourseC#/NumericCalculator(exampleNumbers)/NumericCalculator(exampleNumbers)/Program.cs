1752abde2ef68c514fa2c2bfee155bdd0c1d397b
diego40g <diegopaznaula@hotmail.com>
2022-08-12 00:03:33 -0500

    Use explicit conversion with ReadLine to input ints

---
+++ IntroductionCourseC#/NumericCalculator(exampleNumbers)/NumericCalculator(exampleNumbers)/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, Mundo!");
+
+int number1;
+int number2;
+
+Console.WriteLine("Ingresa el primer número: ");
+number1 = Convert.ToInt32(Console.ReadLine());//Conversión explicita
+
+Console.WriteLine("Ingresa el segundo número: ");
+number2 = Convert.ToInt32(Console.ReadLine());//Conversión explicita
+
+int result = number1 * number2;
+Console.WriteLine("El resultado es: "+result);
