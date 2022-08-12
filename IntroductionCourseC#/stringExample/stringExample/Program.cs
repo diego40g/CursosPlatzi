2ac02ee6ca6079b47363ffcad43678f64dedd832
diego40g <diegopaznaula@hotmail.com>
2022-08-11 23:53:51 -0500

    Example to use string or var to read text

---
+++ IntroductionCourseC#/stringExample/stringExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cual es tu nombre? Escribe tu nombre completo porfavor");
+            //var fullUserName = Console.ReadLine();
+            string fullUserName = Console.ReadLine();
+
+            Console.WriteLine("Bienvenido " + fullUserName + " esto es Platzi");
+        }
+    }
+}
