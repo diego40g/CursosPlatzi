681d5a405cd086463fd8137845f3db5fe9ad3a8a
diego40g <diegopaznaula@hotmail.com>
2022-08-11 22:55:42 -0500

    Console Aplication net core, Hello World

---
+++ IntroductionCourseC#/helloWorldPlatzi/helloWorldPlatzi/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace helloWorldPlatzi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
