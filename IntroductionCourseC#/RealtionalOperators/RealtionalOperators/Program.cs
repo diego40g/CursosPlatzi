40ebdce62a6fa557a6a301b4aa8a7c65b64bbc8b
diego40g <diegopaznaula@hotmail.com>
2022-08-12 23:25:32 -0500

    write text in the result of the relational operators

--- IntroductionCourseC#/RealtionalOperators/RealtionalOperators/Program.cs
+++ IntroductionCourseC#/RealtionalOperators/RealtionalOperators/Program.cs
@@ -6,19 +6,19 @@ int value2 = 18;
 int value3 = -21;
 
 // == igual a
-Console.WriteLine(value1==value2);
+Console.WriteLine("Resultado de "+value1+"=="+value2+": "+(value1==value2));
 
 // != no igual a
-Console.WriteLine(value1!=value2);
+Console.WriteLine("Resultado de " + value1 + "!=" + value2 + ": " + (value1 !=value2));
 
 // > mayor que
-Console.WriteLine(value1 > value2);
+Console.WriteLine("Resultado de " + value1 + ">" + value2 + ": " + (value1 > value2));
 
 // >= mayor o igual que
-Console.WriteLine(value2 >= value3);
+Console.WriteLine("Resultado de " + value2 + ">=" + value3 + ": " + (value2 >= value3));
 
 // < menor que
-Console.WriteLine(value2 < value3);
+Console.WriteLine("Resultado de " + value2 + "<" + value3 + ": " + (value2 < value3));
 
 // <= menor igual que
-Console.WriteLine(value2 <= value3);
+Console.WriteLine("Resultado de " + value2 + "<=" + value3 + ": " + (value2 <= value3));
