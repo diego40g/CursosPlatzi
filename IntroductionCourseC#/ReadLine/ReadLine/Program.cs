d12b94b33d6b79bad8fc48d7f02048fbd5dcb0b5
diego40g <diegopaznaula@hotmail.com>
2022-08-12 23:41:15 -0500

    Data parse of data type to read the information

---
+++ IntroductionCourseC#/ReadLine/ReadLine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el valor de la base del rectangulo. \nPuedes usar decimales ','");
+            float sideA = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el valor de la altura del rectangulo. \nPuedes usar decimales ','");
+            float sideB = float.Parse(Console.ReadLine());
+
+            float result = sideA * sideB;
+            Console.WriteLine("El área del rectangulo es: "+result);
+        }
+    }
+}
