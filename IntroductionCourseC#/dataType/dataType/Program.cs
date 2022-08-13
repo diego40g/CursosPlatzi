ee10392f71946aa691f5a31cca94439f5034047d
diego40g <diegopaznaula@hotmail.com>
2022-08-12 22:38:59 -0500

    Concated string and other data types

---
+++ IntroductionCourseC#/dataType/dataType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dataType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Tipos de datos");
+        int height = 164;
+        int age = 20;
+        string name = "Diego Paz";
+        string information = "La información solicitada es: \n Nombre de usuario " + name+"\n Edad del usuario "+age+"\n Altura del usuario "+height;
+
+        Console.WriteLine(information);
+        
+    }
+}
