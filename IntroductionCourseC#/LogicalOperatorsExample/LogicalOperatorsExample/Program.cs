7c74a70ce527db6f16b3f66f2cbdd8ddca2e55fd
diego40g <diegopaznaula@hotmail.com>
2022-08-12 23:13:30 -0500

    Logical operators AND, OR and NOT

---
+++ IntroductionCourseC#/LogicalOperatorsExample/LogicalOperatorsExample/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool value1 = true;
+bool value2 = true;
+bool value3 = false;
+
+/* && --> AND
+    1  1  =  1      1  1  1 = 1
+    1  0  =  0                0
+    0  1  =  0                0
+    0  0  =  0                0
+*/
+Console.WriteLine("Valor logico AND (&&): " + (value1 && value2 && value3));
+
+/* || --> OR
+    1  1  =  1      1  1  1  = 1
+    1  0  =  1                 1
+    0  1  =  1                 1
+    0  0  =  0      0  0  0  = 0
+*/
+Console.WriteLine("Valor logico OR (||): " + (value1 || value2 || value3));
+
+// ! --> NOT
+Console.WriteLine("Valor logico negación (!): " + !value1);
