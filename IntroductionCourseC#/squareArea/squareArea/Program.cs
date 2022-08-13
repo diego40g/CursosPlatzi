f968100e81d86009ebb865366cff117010b28c3a
diego40g <diegopaznaula@hotmail.com>
2022-08-12 22:55:50 -0500

    arithmetic operators

---
+++ IntroductionCourseC#/squareArea/squareArea/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace squareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calculo area rectangulo
+
+            //valores rectangulo
+            float sideA = 10.2365f;
+            int sideB = 20;
+            sideA++;
+            sideA++;
+            sideB--;
+
+            //area del rectangula altura*base
+            float area = sideA * sideB;
+            Console.WriteLine("El área del rectangulo es: " + area);
+
+            /*operadores aritmeticos
+             * + suma
+             * - resta
+             * * multiplicación
+             * / division
+             * % modulo
+             * ++ aumenta 1
+             * -- resta 1
+             * */
+        }
+    }
+}
