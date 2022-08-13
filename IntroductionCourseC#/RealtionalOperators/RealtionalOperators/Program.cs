d819f30f79bd202cff77c9d248de7ad25120751c
diego40g <diegopaznaula@hotmail.com>
2022-08-12 23:22:11 -0500

    Ralational operators ==, !=, <=,>=

---
+++ IntroductionCourseC#/RealtionalOperators/RealtionalOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Operadores Relacionales");
+
+int value1 = 3;
+int value2 = 18;
+int value3 = -21;
+
+// == igual a
+Console.WriteLine(value1==value2);
+
+// != no igual a
+Console.WriteLine(value1!=value2);
+
+// > mayor que
+Console.WriteLine(value1 > value2);
+
+// >= mayor o igual que
+Console.WriteLine(value2 >= value3);
+
+// < menor que
+Console.WriteLine(value2 < value3);
+
+// <= menor igual que
+Console.WriteLine(value2 <= value3);
