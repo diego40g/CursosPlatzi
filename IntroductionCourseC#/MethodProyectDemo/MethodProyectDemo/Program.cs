823ec061f88049c3afea881fbc30f5069641a34b
diego40g <diegopaznaula@hotmail.com>
2022-08-14 22:07:12 -0500

    method Random

---
+++ IntroductionCourseC#/MethodProyectDemo/MethodProyectDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodProyectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] pricesArray;
+            pricesArray = new float[10];
+            pricesArray[0] = 1.5f;
+            pricesArray[1] = 2.5f;
+            pricesArray[3] = 3f;
+            pricesArray[4] = 5f;
+
+            Random rnd = new Random();
+            Console.WriteLine("Random demo !!!");
+            Console.WriteLine($"{rnd.Next(1, 10)}");
+        }
+    }
+}
