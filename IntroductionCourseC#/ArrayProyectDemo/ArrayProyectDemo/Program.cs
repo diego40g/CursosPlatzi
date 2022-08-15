71056848148f15e8801d46d87ec1fac7912d6bd7
diego40g <diegopaznaula@hotmail.com>
2022-08-14 20:39:08 -0500

    manage arrays of string and float

---
+++ IntroductionCourseC#/ArrayProyectDemo/ArrayProyectDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayProyectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola String");
+
+            string[] coffeTypes;
+            float[] coffeValues;
+
+            coffeTypes = new string[] { "Express", "Largo", "Filtrado", "Latte" };
+            coffeValues = new float[] { 1.2f, 1.5f, 5.0f, 5.5f };
+
+            coffeTypes[1] = "Cargado";
+
+            for(int i=0; i<coffeTypes.Length; i++)
+            {
+                Console.WriteLine("El café "+coffeTypes[i]+" cuesta $" + coffeValues[i]);
+            }
+        }
+    }
+}
