81349e95652fcab3af8b87f3aa63403c8c8ca018
diego40g <diegopaznaula@hotmail.com>
2022-08-16 22:24:43 -0500

    create my methods of addition with void type and multiplication return int

---
+++ IntroductionCourseC#/MyMethod/MyMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyMethod
+{
+    public class Program
+    {
+        public void IntegerAddition(int a, int b)
+        {
+            int addition = a + b;
+            Console.WriteLine("El resultado de la suma de " + a + " + " + b + " = " + addition);
+        }
+        public int IntegerMultiplication(int a,int b)
+        {
+            int multiplication = a * b;
+            return multiplication;
+        }
+        public static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.IntegerAddition(3, 6);
+
+            int result = myProgram.IntegerMultiplication(12, 5);
+            Console.WriteLine("El resultado de la multiplicación de 12 * 5 = " + result);
+        }
+    }
+}
