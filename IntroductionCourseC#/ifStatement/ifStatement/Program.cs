d5ab389351fa4eddac20a030b4cc2ce0a892d6a3
diego40g <diegopaznaula@hotmail.com>
2022-08-20 09:46:19 -0500

    Sentences if and else to manage conditionals

---
+++ IntroductionCourseC#/ifStatement/ifStatement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ifStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anyValue = 6;
+            string message = "";
+
+            if(anyValue == 6)
+            {
+                message = "O por Dios es un milagro el valor es igual a 6";
+            }else if (anyValue == 12)
+            {
+                message = "O por Dios es un doble milagro el valor es 12";
+            }
+            else
+            {
+                message = "Ya nada, el valor no es 6";
+            }
+            Console.WriteLine($"La respuesta es: {message}");
+        }
+    }
+}
