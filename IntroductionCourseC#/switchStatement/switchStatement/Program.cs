fdfb51ecc0ad438b9f7bc4d1e45cafa68bd45e16
diego40g <diegopaznaula@hotmail.com>
2022-08-20 11:07:22 -0500

    switch sentences to control different conditional and default conditional

---
+++ IntroductionCourseC#/switchStatement/switchStatement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class switchStatement
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Seleccioan una soda: ");
+        string caseSwitch = Console.ReadLine();
+
+        switch (caseSwitch)
+        {
+            case "cola":
+                Console.WriteLine("Cola soda - $2 USD");
+                break;
+            case "limon":
+                Console.WriteLine("Limon soda - $1 USD");
+                break;
+            case "naranja":
+                Console.WriteLine("Naranja soda - $1.5 USD");
+                break;
+            case "manzana":
+                Console.WriteLine("Manzana soda - $1 USD");
+                break;
+            default:
+                Console.WriteLine("ERROR: No seleccioanste una soda o ingresaste un valor erroneo.");
+                break;
+
+        }
+    }
+}
