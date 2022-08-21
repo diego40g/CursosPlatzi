d7c802caa82cc043dc2a5a5b46bc842aee6be042
diego40g <diegopaznaula@hotmail.com>
2022-08-21 00:20:50 -0500

    Example of cicle for

---
+++ IntroductionCourseC#/forCicle/forCicle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//Ciclo For ejecuta una isntruccion o varias
+//cada vez que ejecuta un ciclo
+
+namespace forCicle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*condicion inicial*/
+            /*limite de repeticiones*/
+            /*incremento*/
+            for (int i=0; i < 5; i++)
+            {
+                Console.WriteLine($"El numero es #{i}");
+            }
+            Console.WriteLine("-------------------");
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"El numero es #{i}");
+            }
+            Console.WriteLine("-------------------");
+            for (int i = 0; i <= 5; i=i+2)
+            {
+                Console.WriteLine($"El numero es #{i}");
+            }
+        }
+    }
+}
