1109673867a11c20b03e5f42f844c299422e49bb
diego40g <diegopaznaula@hotmail.com>
2022-08-21 19:00:32 -0500

    example of cicle while and do while

---
+++ IntroductionCourseC#/whileCicle/whileCicle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace whileCicle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuarPrograma = true;
+            while (continuarPrograma == true)
+            {
+                Console.WriteLine("¿Desea continuar la ejecucion?\nEscribe 1 para si 0 para no.");
+                int ejecucionPrograma=Convert.ToInt16(Console.ReadLine());
+                if(ejecucionPrograma == 1)
+                {
+                    Console.WriteLine("El programa seguira ejecutandose");
+                }
+                else if (ejecucionPrograma == 0) 
+                {
+                    Console.WriteLine("Es la ultima vez que el programa se a ejecutado");
+                    continuarPrograma = false;
+                }
+                else
+                {
+                    Console.WriteLine("La instrucción ingresada no es valida");
+                }
+                Console.WriteLine("Mensaje por siempre");
+            }
+
+            /*Do while por lo menos se ejecuta 1 vez*/
+            bool condicionDoWhile = false;
+            do
+            {
+                Console.WriteLine("¿Desea continuar la ejecucion?\nEscribe 1 para si 0 para no.");
+                int ejecucionPrograma = Convert.ToInt16(Console.ReadLine());
+                if (ejecucionPrograma == 1)
+                {
+                    Console.WriteLine("El programa seguira ejecutandose");
+                    condicionDoWhile = true;
+                }
+                else if (ejecucionPrograma == 0)
+                {
+                    Console.WriteLine("Es la ultima vez que el programa se a ejecutado");
+                    condicionDoWhile = false;
+                }
+                else
+                {
+                    Console.WriteLine("La instrucción ingresada no es valida");
+                }
+                Console.WriteLine("Mensaje por siempre");
+            } while (condicionDoWhile == true);
+        }
+    }
+}
