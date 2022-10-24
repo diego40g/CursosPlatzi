5116cfa25fb9640859817951861b65320d1a03f5
diego40g <diegopaznaula@hotmail.com>
2022-10-23 23:30:33 -0500

    use and print of variables

--- PracticalCourseJavaScript/ejercicios_prueba/1. varaibles.js
+++ PracticalCourseJavaScript/ejercicios_prueba/1. varaibles.js
@@ -6,3 +6,8 @@ let email='dpaz@email.com'
 let isAdult=true;
 let dineroAhorrado=1000
 let deudas=100
+
+let nombreCompleto=nombre+' '+apellido
+console.log(nombreCompleto);
+let dineroReal=dineroAhorrado-deudas
+console.log(dineroReal);
