02269a5edbbd967f3b21f6708153f527a460cf4d
diego40g <diegopaznaula@hotmail.com>
2022-10-23 23:49:27 -0500

    the last example for and while

--- PracticalCourseJavaScript/ejercicios_prueba/4. cicles.js
+++ PracticalCourseJavaScript/ejercicios_prueba/4. cicles.js
@@ -5,5 +5,14 @@ for (let i=0;i<5;i++){
 let i=0
 while(i<5){
     console.log("El valor de i es: "+i);
-    i=+1;
+    i++;
+}
+
+for (let i=10;i>=2;i--){
+    console.log("El valor de i es: "+i);
+}
+let j=10
+while(j>=2){
+    console.log("El valor de i es: "+j);
+    j--;
 }
