8062c1f525da58e7d793c3151ae4c540dc134b0e
diego40g <diegopaznaula@hotmail.com>
2022-10-27 17:48:58 -0500

    methods of querySelector for select html atributes

--- PracticalCourseJavaScript/course/1. dom/script.js
+++ PracticalCourseJavaScript/course/1. dom/script.js
@@ -1,1 +1,19 @@
-console.log("Hello, word");
+const h1 = document.querySelector('h1');
+const p = document.querySelectorAll('p');
+const claseParrafito = document.querySelector('.parrafito');
+const idParrafo = document.querySelector('#pid');
+const input = document.querySelector('input')
+
+console.log(h1.innerHTML);
+console.log(h1.outerHTML);
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    claseParrafito,
+    idParrafo,
+    input,
+});
+
