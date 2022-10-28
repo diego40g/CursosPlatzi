573a1daa3abea5655cd066c03deb1489c78a6c2b
diego40g <diegopaznaula@hotmail.com>
2022-10-27 21:42:07 -0500

    list events and add numbers

---
+++ PracticalCourseJavaScript/course/2. events/script.js
@@ -0,0 +1,11 @@
+// Selectores
+const h1=document.querySelector('h1');
+const input1=document.querySelector('#calculo1');
+const input2=document.querySelector('#calculo2');
+const btn=document.querySelector('#btnCalcular');
+const pResult=document.querySelector('#result');
+
+function btnOnClick(){
+    const sumaInputs=parseFloat(input1.value)+parseFloat(input2.value);
+    pResult.innerText=`Resultado de ${input1.value} + ${input2.value} = ${sumaInputs}`;
+}
