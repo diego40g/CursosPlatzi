8bf97a1af103b463b75096605dbb66373bd3778f
diego40g <diegopaznaula@hotmail.com>
2022-10-27 21:23:25 -0500

    change data, add attribute, create new tags of HTML

--- PracticalCourseJavaScript/course/1. dom/script.js
+++ PracticalCourseJavaScript/course/1. dom/script.js
@@ -21,3 +21,33 @@ console.log({
     input,
 });
 
+h1.innerText='Patito <br> Inner';
+// h1.innerHTML='Patito <br> Inner';
+// h1.outerHTML='Patito Outer';
+console.log(h1.getAttribute('pantalla'));
+console.log(input.getAttribute('class'));
+input.setAttribute('class','registro')
+console.log(input.getAttribute('class'));
+
+input.classList.add('rojo');
+input.classList.add('verde');
+input.classList.remove('verde');
+input.classList.toggle('rojo');//agrega si tengo o quita si no tengo
+input.classList.contains('rojo');//condicional que devuelve true o false si tiene la clase
+
+
+input.value="456"
+
+//añadir nuevo elemento
+console.log(document.createElement('span'));
+const imagen=document.createElement('img');
+imagen.src="https://josefacchin.com/wp-content/uploads/2020/02/como-quitar-el-fondo-de-una-imagen.png";
+imagen.style.width='520px';
+//imagen.src=('src','https://josefacchin.com/wp-content/uploads/2020/02/como-quitar-el-fondo-de-una-imagen.png');
+console.log(imagen);
+
+idParrafo1.append(imagen);
+/**
+ * idParrafo1.innerHTML="";
+ * idParrafo1.appendChild(imagen); agrega la imagen manteniendo el contenido anterior
+ * */
