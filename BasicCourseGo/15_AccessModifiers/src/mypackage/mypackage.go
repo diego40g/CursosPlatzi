4823efefcfc1c3aea8dd805ac7316180e4140d58
diego40g <diegopaznaula@hotmail.com>
2023-01-07 23:23:25 -0500

    private and public functions

--- BasicCourseGo/15_AccessModifiers/src/mypackage/mypackage.go
+++ BasicCourseGo/15_AccessModifiers/src/mypackage/mypackage.go
@@ -1,7 +1,27 @@
 package mypackage
 
+import "fmt"
+
 // CarPublic car with public access
-type CarPublic struct { 
+type CarPublic struct {
 	Brand string
-	Year int
+	Year  int
+}
+
+// carPrivate car with private access
+type carPrivate struct {
+	brand string
+	year  int
+}
+
+//Print a public function message
+func PrintPublicMessage(text string) {
+	fmt.Println("Mensaje publico")
+	fmt.Println(text)
+}
+
+//Print a private function message
+func printPrivateessage(text string) {
+	fmt.Println("Mensaje privado")
+	fmt.Println(text)
 }
