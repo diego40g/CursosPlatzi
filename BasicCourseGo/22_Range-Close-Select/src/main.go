c980e54d411bac562f08500d48992aacaafd9911
diego40g <diegopaznaula@hotmail.com>
2023-01-10 00:58:07 -0500

    understand some tags

--- BasicCourseGo/22_Range-Close-Select/src/main.go
+++ BasicCourseGo/22_Range-Close-Select/src/main.go
@@ -7,13 +7,16 @@ func message(text string, c chan string) {
 }
 
 func main() {
-	c := make(chan string, 2)
+	c := make(chan string, 3)
 
 	c <- "Mensaje1"
 	c <- "Mensaje2"
-
+	
 	fmt.Println(len(c), cap(c))
-
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(len(c), cap(c))
+	
 	//Close
 	close(c)
 	//c<-"Mensaje3"
@@ -27,10 +30,14 @@ func main() {
 	//Select
 	email1 := make(chan string)
 	email2 := make(chan string)
-	go message("Mensaje1", email1)
-	go message("Mensaje2", email2)
+	go message("Mensaje1-1", email1)
+	go message("Mensaje1-2", email1)
+	go message("Mensaje2-1", email2)
+	go message("Mensaje2-2", email2)
 
-	for i := 0; i < 2; i++ {
+
+
+	for i := 0; i < 4; i++ {
 		select {
 			case m1 := <-email1:
 				fmt.Println("Email recibido de email1", m1)
@@ -38,4 +45,5 @@ func main() {
 				fmt.Println("Email recibido de email2", m2)
 		}
 	}
+
 }
