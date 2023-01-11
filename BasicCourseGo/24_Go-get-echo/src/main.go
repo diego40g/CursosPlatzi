58f601af79ab5dcdf755c5fcb7b0b1b188382e1a
diego40g <diegopaznaula@hotmail.com>
2023-01-10 22:34:52 -0500

    call and implementation a get request using echo

---
+++ BasicCourseGo/24_Go-get-echo/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// instancia echo
+	e := echo.New()
+
+	//ruta
+	e.GET("/", func (c echo.Context) error {
+		return c.String(http.StatusOK, "Hello Word!!!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
