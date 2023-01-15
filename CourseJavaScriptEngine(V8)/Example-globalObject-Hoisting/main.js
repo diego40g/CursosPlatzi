2911e111e420ae422a725976191d47a2ae7ac45c
diego40g <diegopaznaula@hotmail.com>
2023-01-14 19:04:54 -0500

    example to understand the hoisting of javascript enginee

---
+++ CourseJavaScriptEngine(V8)/Example-globalObject-Hoisting/main.js
@@ -0,0 +1,9 @@
+/**
+ * COPIAMOS EL CODIGO Y SALE UNDEFINED Y PAZ POR EL HOISTING DE JS ENGINE
+ * console.log(nombre);
+ * apellido();
+ * var nombre = "Diego"
+ * function apellido() {
+ *  console.log("Paz")
+ * }
+ */
