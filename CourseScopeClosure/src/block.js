248425ff7e942497e4bf44be883a247158b04c2e
diego40g <diegopaznaula@hotmail.com>
2023-03-18 20:56:39 -0500

    block is into of "{ }" and tipe var has a global scope

---
+++ CourseScopeClosure/src/block.js
@@ -0,0 +1,18 @@
+function fruits() {
+    if (true) {
+        var fruit1 = 'Apple';
+        let fruit2 = 'Kiwi';
+        const fruit3 = 'Banana';
+        console.log(fruit1);
+        console.log(fruit2);
+        console.log(fruit3);
+    }
+    console.log(fruit1);
+    // console.log(fruit2);
+    // console.log(fruit3);
+}
+
+fruits()
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
