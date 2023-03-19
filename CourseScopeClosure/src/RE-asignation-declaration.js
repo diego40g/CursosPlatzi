449d14d347dd2943174f646d485c91cde2cf65ce
diego40g <diegopaznaula@hotmail.com>
2023-03-18 22:23:48 -0500

    differents of var, let and const

---
+++ CourseScopeClosure/src/RE-asignation-declaration.js
@@ -0,0 +1,30 @@
+//VAR
+var firstName; //Declaration - undefined
+firstName = 'Diego' //asignation
+console.log(firstName)
+
+var lastName = 'Ignacio' //Declaration and asignation
+lastName = "Paz" //RE-asignation
+console.log(lastName)
+
+var secondName = 'Diego' //Declaration and asignation
+var secondName = 'Ignacio' //RE-declaration
+console.log(secondName)
+
+//LET
+let fruit = 'Apple' // Declaration and asignation
+fruit = 'Kiwi'  //RE-asignation
+
+let fruit = 'Banana'  //RE-declaration no is possible
+
+//CONST
+const animal = 'dog' // Declaration and asignation
+animal = 'cat' //RE-asignation no is possible
+const animal = 'cat' //RE-declaration no is possible
+
+const vehicles = []
+vehicles.push("🚙")
+console.log(vehicles)
+
+vehicles.pop()
+console.log(vehicles)
