a84b8238fe228844f6e3b4c2a996d188a001ca94
diego40g <diegopaznaula@hotmail.com>
2023-03-18 20:19:46 -0500

    function scope example

---
+++ CourseScopeClosure/src/function.js
@@ -0,0 +1,11 @@
+function greeting() {
+    let userName = 'Diego'
+    console.log(userName)
+
+    if(userName === 'Diego'){
+        console.log(`Hello ${userName}`)
+    }
+}
+
+greeting()
+console.log(userName)
