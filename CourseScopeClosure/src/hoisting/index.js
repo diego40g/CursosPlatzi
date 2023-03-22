9aa4fc85225fe8a71e30389d1707c7665c7ab2fb
diego40g <diegopaznaula@hotmail.com>
2023-03-22 16:38:48 -0500

    understand hoisting

---
+++ CourseScopeClosure/src/hoisting/index.js
@@ -0,0 +1,13 @@
+//VARIABLE
+
+console.log(nameOfDog)
+var nameOfDog = 'Perrito'
+
+//FUNCIÓN
+/*nameOfDog()
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${nameDog}`)
+}
+
+var nameDog = 'Fenry'*/
