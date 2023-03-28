9c3c904fbb2f3899b6c2274c4679c0e692b0ab6f
diego40g <diegopaznaula@hotmail.com>
2023-03-27 22:30:28 -0500

    corrections and using fetchdata in examples

--- CourseAsynchronousJavascript/src/callback/challenge.js
+++ CourseAsynchronousJavascript/src/callback/challenge.js
@@ -8,12 +8,25 @@ function fetchData(urlApi, callback){
     xhttp.onreadystatechange = (event) => {
         if(xhttp.readyState === 4){
             if(xhttp.status === 200){
-                callback(null, JSON.parse(xhttp.resposeText));
+                callback(null, JSON.parse(xhttp.responseText));
+            }else{
+                const error = new Error('Error' + urlApi);
+                return callback(error, null)
             }
-        }else{
-            const error = new Error('Error' + urlApi);
-            return callback(error, null)
         }
     }
     xhttp.send();
 }
+
+fetchData(`${API}/products`, (error1, data1)=>{
+    if(error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, (error2, data2) => {
+        if(error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, (error3, data3) => {
+            if(error3) return console.error(error3);
+            console.log(data1[0])
+            console.log(data2.title)
+            console.log(data3.name)
+        })
+    })
+})
