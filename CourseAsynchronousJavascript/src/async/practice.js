4df00ab22c8f3609bbb7290c6b9801c6cee67c09
diego40g <diegopaznaula@hotmail.com>
2023-03-30 00:13:57 -0500

    the server solution

--- CourseAsynchronousJavascript/src/async/practice.js
+++ CourseAsynchronousJavascript/src/async/practice.js
@@ -1,4 +1,14 @@
 import fetch from "node-fetch";
+/*
+export async function runCode() {
+  const url = 'https://domain-api-com';
+  try {
+    await fetch(url)
+  } catch (error) {
+    throw new Error('API Not Found');
+  }
+}
+ */
 
 async function runCode() {
     // Tu código aquí 👈
