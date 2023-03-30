512948a134cdfbf98477b8f08c9936c110766609
diego40g <diegopaznaula@hotmail.com>
2023-03-29 23:44:43 -0500

    challenge to do GET request with async and await

---
+++ CourseAsynchronousJavascript/src/async/challenge.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+const API = "https://api.escuelajs.co/api/v1"
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+    return data;
+}
+
+const anotherFn = async(urlApi) => {
+    try {
+        const products = await fetchData(`${API}/products`)
+        const product = await fetchData(`${API}/products/${products[0].id}`)
+        const category = await fetchData(`${API}/categories/${product.category.id}`)
+
+        console.log(products[0])
+        console.log(product.title)
+        console.log(category.name)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+anotherFn()
