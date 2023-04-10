59b2446e98c6dadc6daabfe6f51658ec2492b138
diego40g <diegopaznaula@hotmail.com>
2023-04-10 17:59:07 -0500

    understand what is typing

---
+++ CourseFoundamentalsTypescript/002_dataTypes/src/03_typing.ts
@@ -0,0 +1,14 @@
+(() => {
+  let myProductName: string = 'Product example';
+  let myProductPrice: number = 123;
+  
+  myProductName = 'ChAnGe';
+  myProductName.toLowerCase();
+  
+  myProductPrice.toFixed();
+  
+  const myProductMaxStock = 1000;
+  const myProductDefaultDescription = 'Descrición por defecto';
+}) ();
+
+
