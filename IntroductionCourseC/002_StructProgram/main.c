5f3ec7987fa2688fdf961d69dcd6ed2509497f7d
diego40g <diegopaznaula@hotmail.com>
2023-04-30 22:20:12 -0500

    comments and the general struct of main in c

---
+++ IntroductionCourseC/002_StructProgram/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int mian(){
+    //One comment line
+    /*First hello world in C language*/
+    
+    //all instructs of main
+    printf("Hello friends :)");
+    
+    //the last instruct in c
+    return 0;
+}
