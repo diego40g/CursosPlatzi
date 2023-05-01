631de775ac5aec8e6766a0e6bd9da41a697c65be
diego40g <diegopaznaula@hotmail.com>
2023-04-30 22:57:17 -0500

    understand integer and unsigned integer with a basic example

---
+++ IntroductionCourseC/003_BasicTypes/Artimetics/Integers/aritmeticInt.c
@@ -0,0 +1,23 @@
+/*
+    Basic types:
+    (A) Aritmeticos int
+        2 o 4 bytes (actualmente son solo 4 bytes)
+        -32 768 a 32 767
+        -2 147 483 648 a 2 147 483 647
+
+    Los unsigned int
+        0 a 65 535
+        0 a 4 294 967 295
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num1 = 4294967123;
+    unsigned int num2 = 4294967123;
+
+    printf("Integer 1: %d", num1);
+    printf("Integer 1: %d", num2);
+
+    return 0;
+}
