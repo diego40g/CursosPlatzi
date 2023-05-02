e88d6943b58f67407660d2b93e36464cd0ab894e
diego40g <diegopaznaula@hotmail.com>
2023-05-01 20:03:07 -0500

    example and understand void data type

---
+++ IntroductionCourseC/003_BasicTypes/Void/voidDataType.c
@@ -0,0 +1,21 @@
+/*
+    Tipo de dato void
+    no hay valor disponible
+    Uso:
+    1.- Una funcion retorna un tipo void 
+    2.- Función con argumentos tipo void
+    3.- Puntero o apuntador que va a la direccion en memoria de un objeto, pero no le importa el tipo de dato que tenga
+*/
+
+//1
+void printName () {
+
+}
+
+//2
+int randomNumber(){
+
+    return 4;
+}
+
+//3
