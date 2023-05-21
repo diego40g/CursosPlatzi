55093e12879c598bcfe5f6666fd8afc4e1cf982a
diego40g <diegopaznaula@hotmail.com>
2023-05-20 22:59:52 -0500

    Example of adding stack overflow example

---
+++ IntroductionCourseC/004_variables/Declaration/exampleVariables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Varaible declarations
+int a, b, c;
+float f, g, h;
+
+int main(){
+    //Variable initialization
+    a = 1;
+    b = 12332564123891845187;
+    c = a + b;
+    printf("Value of sum is = %d", c);
+
+    return 0;
+}
