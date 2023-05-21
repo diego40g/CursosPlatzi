aa9a53bb23e6b1bc0de801841a865c36ef42e006
diego40g <diegopaznaula@hotmail.com>
2023-05-20 23:51:46 -0500

    doing a extern varaiables

---
+++ IntroductionCourseC/004_variables/Declaration/externVariables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Varaible declarations
+extern int a, b, c;
+float f, g, h;
+
+int main(){
+    //Variable definition
+    int a, b, c;
+
+    //Variable initialization
+    a = 0;
+    b = 34;
+    c = a + b;
+    printf("Value of sum is = %d", c);
+
+    return 0;
+}
