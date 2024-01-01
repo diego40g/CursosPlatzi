cd9c3fe0f752c181a69d07756cc0699cdbcccfce
diego40g <diegopaznaula@hotmail.com>
2023-12-31 23:06:57 -0500

    main to understand the use of variables on java

---
+++ IntroductionCourseJavaEE/understandVariables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int speed;
+        speed = 0;
+
+        System.out.println(speed);
+
+        int salary = 1200;
+        String employeeName = "Diego";
+        System.out.println(employeeName + " has a salary of " + salary);
+    }
+}
